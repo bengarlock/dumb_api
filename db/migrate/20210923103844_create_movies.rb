d97f82b95da04d13a85cd1d85f7f31245f721182
bengarlock <ben.garlock@gmail.com>
2021-09-23 07:35:42 -0400

    first commit

---
+++ db/migrate/20210923103844_create_movies.rb
@@ -0,0 +1,13 @@
+class CreateMovies < ActiveRecord::Migration[6.1]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.string :year
+      t.string :genre
+      t.string :score
+      t.string :imageUrl
+      t.timestamps
+    end
+  end
+end
+
